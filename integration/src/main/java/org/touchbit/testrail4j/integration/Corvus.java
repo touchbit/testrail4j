ce786744add803b07528a54b24be25f610989b8b
Oleg Shaburov <oshaburov@ozon.ru>
2019-01-05 01:48:32 +0300

    fix NPE

--- integration/src/main/java/org/touchbit/testrail4j/integration/Corvus.java
+++ integration/src/main/java/org/touchbit/testrail4j/integration/Corvus.java
@@ -21,6 +21,7 @@ public class Corvus extends Buggy {
 
     public static void main(String[] args) {
         setProgramName("Corvus");
+        Buggy.prepare(args);
         disableWarning();
         waitMigrations();
         Buggy.main(args);
